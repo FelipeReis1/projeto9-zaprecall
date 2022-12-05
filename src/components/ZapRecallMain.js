a50f4778bc82cf5f1644ffe4c37efd72ecb8dcc9
FelipeReis1 <feliperg15@yahoo.com.br>
2022-12-04 22:26:34 -0300

    adicionado GlobalStyle - projeto finalizado

--- src/components/ZapRecallMain.js
+++ src/components/ZapRecallMain.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 import Logo from "../components/Logo/Logo";
 import Cards from "./Cards";
-
+import GlobalStyle from "../assets/styles/GlobalStyle";
 export default function ZapRecallMain() {
   return (
     <StyledContainer>
+      <GlobalStyle />
       <Logo />
       <Cards />
     </StyledContainer>
